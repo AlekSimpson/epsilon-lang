cf7bcad46d11e7c87d305a7eef4668874cf31b29
AlekSimpson <alek@asu.me>
2023-01-13 10:19:27 -0700

    tests work now

--- tests/parser/test_cases.jl
+++ tests/parser/test_cases.jl
@@ -1,47 +1,47 @@
 include("../Validator.jl")
 include("../../src/node.jl")
 
-add_op_correct = BinOpNode(NumberNode(Token("NUMBER", "1")), 
+add_op_correct = [BinOpNode(NumberNode(Token("NUMBER", "1")), 
                            Token("ADD", "+"), 
-                           NumberNode(Token("NUMBER", "2")))
+                           NumberNode(Token("NUMBER", "2")))]
 add_op = Test("Addition Operator", 
               [Token("NUMBER", "1"),
                Token("ADD", "+"), 
                Token("NUMBER", "2")],
               add_op_correct)
 
-sub_op_correct = BinOpNode(NumberNode(Token("NUMBER", "1")), 
+sub_op_correct = [BinOpNode(NumberNode(Token("NUMBER", "1")), 
                            Token("SUB", "-"), 
-                           NumberNode(Token("NUMBER", "2")))
+                           NumberNode(Token("NUMBER", "2")))]
 sub_op = Test("Subtraction Operator", 
               [Token("NUMBER", "1"),
                Token("SUB", "-"), 
                Token("NUMBER", "2")],
               sub_op_correct)
 
-mult_op_correct = BinOpNode(NumberNode(Token("NUMBER", "1")), 
+mult_op_correct = [BinOpNode(NumberNode(Token("NUMBER", "1")), 
                            Token("MULT", "*"), 
-                           NumberNode(Token("NUMBER", "2")))
+                           NumberNode(Token("NUMBER", "2")))]
 mult_op = Test("Multiplication Operator", 
                [Token("NUMBER", "1"),
                 Token("MULT", "*"), 
                 Token("NUMBER", "2")],
                mult_op_correct)
 
-div_op_correct = BinOpNode(NumberNode(Token("NUMBER", "1")), 
+div_op_correct = [BinOpNode(NumberNode(Token("NUMBER", "1")), 
                            Token("DIV", "/"), 
-                           NumberNode(Token("NUMBER", "2")))
+                           NumberNode(Token("NUMBER", "2")))]
 div_op = Test("Division Operator", 
               [Token("NUMBER", "1"),
-               Token("MULT", "/"), 
+               Token("DIV", "/"), 
                Token("NUMBER", "2")],
               div_op_correct)
 
-TDO_correct = BinOpNode(NumberNode(Token("NUMBER", "1")), 
+TDO_correct = [BinOpNode(NumberNode(Token("NUMBER", "1")), 
                          Token("ADD", "+"), 
                          BinOpNode(NumberNode(Token("NUMBER", "2")), 
                                    Token("MULT", "*"), 
-                                   NumberNode(Token("NUMBER", "4"))))
+                                   NumberNode(Token("NUMBER", "4"))))]
 
 two_diff_ops = Test("Two Different Operators", 
                     [Token("NUMBER", "1"), 
@@ -52,4 +52,4 @@ two_diff_ops = Test("Two Different Operators",
                    , TDO_correct) 
 
 
-parser_tests = [add_op]#, sub_op, mult_op, div_op, two_diff_ops] 
+parser_tests = [add_op, sub_op, mult_op, div_op, two_diff_ops]
